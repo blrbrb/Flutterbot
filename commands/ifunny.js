ddb7793e0cc2b3e1c83e595c523515c238b918aa
Eli <elireynolds23@gmail.com>
2022-05-25 01:43:15 -0400

    Fixed .gitignore

--- commands/ifunny.js
+++ commands/ifunny.js
@@ -1,32 +1,84 @@
-var Scraper = require('images-scraper');
-
-const google = new Scraper({
-puppeteer: {
-    headless:true
-}
-})
+var request = require('request')
+var cheerio = require('cheerio')
 
 module.exports = {
-name: 'image',
-description: 'sends a random image corresponding to a google image search',
-    async execute(client, message, args, debug)
-    {
-       // message.channel.send(scraper);
-        const image_query = args.join(" ");
-        console.log(image_query);
-        if(debug) {
-            message.channel.send('fetching with kq! =' + image_query);
-            message.channel.send('running scripts...');
-        }
+    name: 'ifunny',
+    description: 'god help you',
+    execute(message, args) {
 
-        if(!image_query) return message.channel.send('Please Supply a SearchTerm');
-        
-        const image_results = await google.scrape(image_query, 1);
-        message.channel.send(image_results[0].url);
-        
-        if(debug)
-        {
-            message.channel.send('successfully scraped:' + " " + image_results.length + " " +'image results');
+        // console.log('working');
+        var def = 'https://ifunny.co/tags/';
+
+        if (args <= 0) {
+            var options = {
+                url: "https://ifunny.co/other",
+                method: "GET",
+                headers: {
+                    "Accept": "text/html",
+                    "User-Agent": "Chrome"
+                }
+            };
+
         }
-    }
-}
+        else
+
+            var options = {
+                url: "https://ifunny.co/tags/" + args,
+                method: "GET",
+                headers: {
+                    "Accept": "text/html",
+                    "User-Agent": "Chrome"
+                }
+            };
+
+
+        request(options, function (error, response, responseBody) {
+
+            if (error) { console.log('Im yellow shy, and Eli is a useless, incapable human being'); return; }
+
+            $ = cheerio.load(responseBody);
+            //_26gD._3_Go._3brC.
+            //This line will change often as Ifunny updates it's dumb API 
+            var links = $("._0ZvA img");
+
+
+            // document.querySelector("#App > div._2Uj_ > div._2kNu > div > div._26gD._3_Go > div:nth-child(1) > div > div.l5Cl > img")
+
+            var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("data-src"));
+
+            //message.channel.send(links[1]);
+
+
+
+            console.log(urls);
+            if (!urls.length) {
+                message.channel.send('invalid array allocation: urls.length < 0');
+                //message.channel.send(urls[1]);
+
+
+
+
+                console.log('you fucked it up, objectvie B kill all humans');
+
+                return;
+            }
+
+
+            //filter through array, send resulting random image
+            else {
+                message.channel.send("p-please don't make me look through ifunny");
+                urls.shift();
+                message.channel.send(urls[1]);
+                return;
+            }
+
+
+        });
+
+
+
+
+
+
+
+

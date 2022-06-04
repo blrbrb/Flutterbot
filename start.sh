e2605ffe1826b6811ec59cad05830525d3a2b8e6
Eli <elireynolds23@gmail.com>
2022-06-04 18:10:33 -0400

    improved startup script + error logs

--- start.sh
+++ start.sh
@@ -1,13 +1,39 @@
 # /bin/sh
+
+
+
+FILE=error_log.txt
+CLEARSIZE=500000;
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+
+    FILESIZE=$(stat -c%s "$FILE") 
+
+        if ((FILESIZE > CLEARSIZE )); then 
+            echo "error_log file has reached maximum size, clearing memory.." 
+            > error_log.txt 
+        fi
+else 
+    echo "$FILE does not exist. Initalizing javascript error log..." 
+    touch error_log.txt 
+fi
+
+
 while true 
 
 do 
 
 echo Starting Fluttershy 
+if [ -f "$FILE" ]; then 
+     node . 2> error_log.txt 
+     
 
-node . 
+else 
+    node . 
+fi  
 
 echo Restarting Fluttershy in 10 Seconds... 
+
 
 sleep 10 
 

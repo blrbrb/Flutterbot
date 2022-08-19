f310b610dca5122fa0fa2549c2bb246662cabb3c
Eli <elireynolds23@gmail.com>
2022-08-18 20:32:52 -0400

    even more tweaks

--- commands/fluttershy.js
+++ commands/fluttershy.js
@@ -23,25 +23,45 @@ module.exports = {
 
         message.channel.startTyping();
         console.log(message.content);
-        let input = message.content.slice(prefix.length + command_prefix.length).trim(); 
-        console.log(input); 
+        let input = message.content.slice(prefix.length + command_prefix.length).trim();
+        let conversation = [];
+
+        if (conversation.length < 1) {
+            const conversationdata = fs.readFileSync('assets/conversation.json');
+            conversation = JSON.parse(conversationdata);
+            if (!conversation.length) {
+                conversation = [];
+            }
+        }
+
+
+        console.log(input);
+
         const payload = {
             inputs: {
+                past_user_inputs: conversation, 
                 text: message.content
             }
         };
 
-
+        console.log(conversation); 
         const response = await fetch(API_URL, {
             method: 'post',
             body: JSON.stringify(payload), 
-            headers: headers
+            headers: { Authorization: `Bearer ${process.env.HUGGING_TOKEN}` },
         }
         );
         const data = await response.json();
         let bot_reply = ' ';
         if (data.hasOwnProperty('generated_text')) {
             bot_reply = data.generated_text;
+            conversation.push(input); 
+            conversation.push(data.generated_text); 
+        }
+        else if (data.hasOwnProperty('generated_responses'))
+        {
+            console.log(data.generated_responses);
+            bot_reply = data.generated_responses; 
         }
         else if (data.hasOwnProperty('error'))
         {
@@ -50,6 +70,16 @@ module.exports = {
             console.log(data.error); 
             message.reply('there was an error'); 
         }
+
+
+
+        const json = JSON.stringify(conversation);
+
+        fs.writeFile("assets/conversation.json", json, function (err, result) {
+
+            if (err) console.log('JSON file writing error in FlutterShy.js caught', err);
+
+        });
 
         message.channel.stopTyping();
 

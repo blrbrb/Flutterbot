5ab48004fb83cd14496ed2a576490afa056c8a21
NekoNoka <50730918+NekoNoka@users.noreply.github.com>
2023-08-26 21:35:20 -0700

    stupid mistake fixed

--- utils/findFiles.js
+++ utils/findFiles.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = function (dirname, relativePath, endsWith = '') {
-    return fs.readdirrSync(path.join(dirname, relativePath)).filter(folder => !folder.startsWith('_') && !folder.startsWith('.DS_STORE') && folder.endsWith(endsWith));
+    return fs.readdirSync(path.join(dirname, relativePath)).filter(folder => !folder.startsWith('_') && !folder.startsWith('.DS_STORE') && folder.endsWith(endsWith));
 }

10c43136a91509dd1480b59b1a06d91a0ed14d85
Eli <58314051+blrbrb@users.noreply.github.com>
2024-04-19 16:58:19 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-
-
-const {flutterbot} = require('./client/Flutterbot.js');
-//const register = require('./registerSlashCommands.js');
-//register; uncomment this to re-register slash commands at bot startup 
-flutterbot.start();
